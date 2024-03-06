0010c58cbef026123506cd7c16b316568eed6ee9
Zainab Al-Najjar <eng.zainab.alnajar@gmail.com>
2024-03-07 02:14:55 +0300

    Tried the import & export methods

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,23 @@
 import React from "react";
-import howManyParks from "./parks/howManyParks";
+import aDifferentName from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import * as RMFunctions from "./parks/RockyMountain";
+
+console.log(RMFunctions.trees);
+// => "Aspen and Pine"
+
+RMFunctions.wildlife();
+// => "Elk, Bighorn Sheep, Moose"
+
+RMFunctions.elevation();
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
-
-  return <h1>Colorado State Parks!</h1>;
-}
+  aDifferentName(); // => "42 parks!"
+    return (
+      <div>
+        <MesaVerde />
+      </div>
+    );
+  }
+  
+  export default ColoradoStateParks;
